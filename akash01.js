1e827a428eff0dd55bfd001460b61ed8673bcb6a
AKASHDESAI75 <akash.rabari.cg@gmail.com>
2024-09-28 11:46:32 +0530

    five commmit

---
+++ akash01.js
@@ -0,0 +1,14 @@
+// let student=83;
+
+
+// if(student>90){
+//     console.log("A");
+// } else if(student<=89&&student>80){
+//     console.log("B");
+// } else if(student<=79&&student>70){
+//     console.log("C");
+// } else if(student<=69&&student>60){
+//     console.log("D");
+// } else {
+//     console.log("F");
+// }
