77c43a1e84f90462b9576da17fbe820f9cba04ea
AKASHDESAI75 <akash.rabari.cg@gmail.com>
2024-10-01 11:42:35 +0530

    six commit

---
+++ if else .js
@@ -0,0 +1,236 @@
+// let student=83;
+
+
+// if(student>90){
+//     console.log("A");
+// } else if(student<=89&&student>80){
+//     console.log("B");
+// } else if(student<=79&&student>70){
+//     console.log("C");
+// } else if(student<=69&&student>60){
+//     console.log("D");
+// } else {
+//     console.log("F");
+// }
+
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+// let i;
+// let j;
+
+// i=1;
+// while(i<=4){
+//     let r='';
+//     j=1;
+//     while(j<=i){
+//         r=r+i;
+//         j++;
+//     }
+//     console.log(r);
+//     i++;
+// }
+
+
+
+// let student=83;
+
+
+// if(student>90){
+//     console.log("A");
+// } else if(student<=89&&student>80){
+//     console.log("B");
+// } else if(student<=79&&student>70){
+//     console.log("C");
+// } else if(student<=69&&student>60){
+//     console.log("D");
+// } else {
+//     console.log("F");
+// }
+
+// function check(student){
+//     if(student>=80){
+//         console.log("positive");
+//     }else if(student<=69&&student>=50){
+//         console.log("negaitv");
+//     }else{
+//         console.log("zero");
+//     }
+// }
+
+
+// let student=50;
+// check(student)
+
+
+// function check(A , B, C){
+//     if(A == B && B == C){
+//         console.log("equilateral triangle");
+//     }else if(A == B || B == C || A == C) {
+//         console.log("isosceles");
+//     }else{
+//         console.log("scalene");
+//     }
+// }
+
+// check(5,5,5)
+
+
+
+
+// function check(ch){
+
+//     if(ch=='a'|| ch=='e'|| ch=='i' || ch=='o' || ch=='u'
+//     ||ch=='A' || ch=='E' || ch=='i' || ch=='O' || ch=='U'){
+//         console.log("vowel");
+//     } else {
+//         console.log("consonant");
+//     }
+//    }
+//    let ch = 'A';
+//     check(ch)
+
+
+// function check(number){
+//     if(number>18){
+//         console.log("eligible for voat");
+//     } else {
+//         console.log("not eligible for voat")
+//     }
+// }
+//  let number = 20;
+//  check(number)
+
+// function check(username,password){
+//     if(username=="admin"&& password=="1234"){
+//         console.log("Login successful");
+//     }else {
+//         console.log("Login failed");
+//     }
+// }
+// let username="admin";
+// let password="1234678";
+
+// check(username,password)
+
+
+
+// function check(bus){
+//     if(bus=="red"){
+//         console.log("Stop");
+//     }else if(bus=="yellow"){
+//         console.log("Slow down");
+//     }else if(bus=="green"){
+//         console.log("Go");
+//     }else {
+//         console.log("invalid number");
+//     }
+    
+// }
+// let bus= "pink";
+// check(bus)
+
+
+
+// 1........
+
+// function check(number){
+//     if(number==1){
+//         console.log("one");
+//     }else if(number==2){
+//         console.log("two");
+//     }else if(number==3){
+//         console.log("three");
+//     }else if(number==4){
+//         console.log("four");
+//     }else if(number==5){
+//         console.log("five");
+//     }else if(number==6){
+//         console.log("six");
+//     }else if(number==7){
+//         console.log("seven");
+//     }else if(number==8){
+//         console.log("eight");
+//     }else if(number==9){
+//         console.log("nine");
+//     }else{
+//         console.log("not valid")
+//     }
+// }
+ 
+//  let number=1;
+//  check(number)
+
+
+// 2..........
+
+// function check(number){
+//     if(number>=5.00&&number<=11.59){
+//         console.log("Good morning");
+//     }else if(number>=12.00&&number<=17.59){
+//         console.log("Good afternoon");
+//     }else if(number>=18.00&&number<=21.59){
+//         console.log("Good evening");
+//     } else if(number>=22.00&&number<=4.59){
+//         console.log("Good night");
+//     }
+// }
+
+// let number=15.45;
+// check(number)
+
+
+
+// 3.........
+
+// function check(num1,num2,calculator){
+//     if(calculator=='+'){
+//         console.log(num1+num2);
+//     }else if(calculator=='-'){
+//         console.log(num1-num2);
+//     }else if(calculator=='*'){
+//         console.log(num1*num2);
+//     }else if(calculator=='/'){
+//          if(num2==0){
+//             console.log("error");
+//         }
+//         console.log(num1/num2);
+//     }else{
+//         console.log("visible")
+//     }
+// }
+
+// check(15 , 12 , '+')
+
+
+// 6.......
+// let i;
+//  for(i=1;i<=100;i++){
+//      if(i % 3 == 0){
+//          console.log("Fizz");
+//      }else if(i % 5 == 0){
+//          console.log("Bizz");
+//      }else if(i % 3 == 0 && i % 5 == 0){
+//          console.log("FizzBizz");
+//      }else{
+//          console.log(i)
+//      }
+//  }
+
+
+
+
+// 5.......
+
+// function check(num1 , num2 , num3){
+//     if(num1>=num2&&num1<=num3){
+//         console.log(num1);
+//     }else if(num1<=num2&&num2<=num3){
+//         console.log(num2);
+//     }else{
+//         console.log(num3);
+//     }
+// }
+
+// check(10,45,56);
+ 
